a5d7053bf73a68c8dd469b6d04da42ac78d786ab
Raafi <92612605+Raafina@users.noreply.github.com>
2023-07-21 14:17:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ from sklearn.linear_model import LinearRegression
 model = pickle.load(open('model.pkl', 'rb'))
 
 # Title
-st.title('Prediksi Index Sewa')
-st.write('Nama : Raafi Nur Adzani')
-st.write('NIM : A11.2021.13536')
+st.title('Cost of Living Index Prediction')
+st.image('img.jpg', use_column_width=True)
+st.write('The Cost of Living Index is a relative indicator of consumer goods prices, including groceries, restaurants, transportation and utilities. The Cost of Living Index does not include accommodation expenses such as rent or mortgage. If a city has a Cost of Living Index of 120, it means Numbeo has an estimated cost that is 20% more expensive than New York (excluding rent).')
 
 col1, col2 = st.columns(2)
 
@@ -24,16 +24,16 @@ def is_numeric(value):
         return False
 # C:\Users\asd\TA_Datamining>streamlit run main.py
 # Input
-Indeks_Sewa = st.text_input("Masukkan Indeks Sewa: ")
-Indeks_Grosir = st.text_input("Masukkan Indeks Grosir: ")
-Indeks_Harga_Restoran = st.text_input("Masukkan Harga Restoran: ")
-Indeks_Daya_Beli_Lokal = st.text_input("Masukkan Daya Beli Lokal: ")
+Indeks_Sewa = st.text_input("Input Rent Index: ")
+Indeks_Grosir = st.text_input("Input Groceries Index: ")
+Indeks_Harga_Restoran = st.text_input("Input Restaurant Price Index: ")
+Indeks_Daya_Beli_Lokal = st.text_input("Input Local Purchasing Power Index:  ")
 
 # prediksi
 prediksi_Indeks_biaya_hidup = ''
 
 # tombol untuk prediksi
-if st.button('Prediksi'):
+if st.button('Prediction'):
     # Validate inputs as numeric values
     if is_numeric(Indeks_Sewa) and is_numeric(Indeks_Grosir) and is_numeric(Indeks_Harga_Restoran) and is_numeric(Indeks_Daya_Beli_Lokal):
         # Convert inputs to float
